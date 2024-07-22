234f7730c1f3fc4bd8a0cb6b348013444d2a5d2f
ChristianCella <christian.cella@polimi.it>
2024-07-22 10:50:50 +0200

    Modified the snippet GetAllOperationsCreated.cs

--- Human/GetAllOperationsCreated.cs
+++ Human/GetAllOperationsCreated.cs
@@ -12,19 +12,24 @@ public class MainScript
    
     public static void MainWithOutput(ref StringWriter output)
     {
-
-        // Initialize a variable taht contains the root of the operations
+    
+    	// Initialize a variable taht contains the root of the operations
     	TxOperationRoot variable = TxApplication.ActiveDocument.OperationRoot;
     	
     	// Specify the type of operations that we want to get
     	TxTypeFilter filter = new TxTypeFilter(typeof(ITxOperation));
         filter.AddIncludedType(typeof(TxHumanTsbSimulationOperation));
     	filter.AddIncludedType(typeof(TxContinuousRoboticOperation));
-
-        // Get the list of operations: do not use 'GetAllDescendants' because it also gives the points inside the operations
+    	
+    	// Get the list of operations: do not use 'GetAllDescendants' because it also gives the points inside the operations
     	TxObjectList List = variable.GetDirectDescendants(filter);
- 					
-		// Display the names to check if they are correct
-        output.Write("The first operation is: " + List[1].Name.ToString());
+    	
+    	// Display the names to check if they are correct
+    	for (int ii = 0; ii < List.Count; ii ++)
+    	{
+    		int new_idx = ii + 1;
+    		output.Write("The opeeration number " + new_idx + " is called: " + List[ii].Name.ToString() + "\n");
+    	}
+ 
     }
 }
