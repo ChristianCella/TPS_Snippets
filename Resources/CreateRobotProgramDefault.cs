4dd9edcacf7c46eb90fbec68e3da0cd21766c7c3
ChristianCella <christian.cella@polimi.it>
2025-02-05 17:04:23 +0100

    Added the complete snippet for pick and place with suxction cup

--- Resources/CreateRobotProgramDefault.cs
+++ Resources/CreateRobotProgramDefault.cs
@@ -2,7 +2,7 @@
 This snippet allows to create a robot program from zero (and it's the equivalent of importing a program from the
 simulator). 
 It also allows to add waypoints in specific positions and set their parameters.
-Finally, it allows to add OLP commands to close the gripper in a specific waypoint.
+Finally, it allows to add OLP commands to attach an oabject to the robot in a specific waypoint.
 */
 
 using System.Collections;
@@ -21,7 +21,7 @@ public class MainScript
     {
 
 		// Define some variables
-		string operation_name = "RoboticProgram1";
+		string operation_name = "Test_program";
 
 		string new_tcp = "tgripper_tf";
     	string new_motion_type = "PTP";
@@ -35,6 +35,13 @@ public class MainScript
     	// Save the robot (the index may change)  	
     	TxObjectList objects = TxApplication.ActiveDocument.GetObjectsByName("GoFa12");
     	var robot = objects[0] as TxRobot;
+
+		// Get the object to attach to the tool (and the tool)
+		ITxObject item = TxApplication.ActiveDocument.
+		GetObjectsByName("Middle_box_camozzi_1")[0];
+
+		ITxObject tool = TxApplication.ActiveDocument.
+		GetObjectsByName("Suction cup")[0];
     	   	
     	// Create the new operation    	
     	TxContinuousRoboticOperationCreationData data = new TxContinuousRoboticOperationCreationData(operation_name);
@@ -53,10 +60,18 @@ public class MainScript
         
         TxRoboticViaLocationOperationCreationData Point3 = new TxRoboticViaLocationOperationCreationData();
         Point3.Name = "point3"; // Third point
+
+		TxRoboticViaLocationOperationCreationData Point4 = new TxRoboticViaLocationOperationCreationData();
+        Point4.Name = "point4"; // Fourth point
+
+		TxRoboticViaLocationOperationCreationData Point5 = new TxRoboticViaLocationOperationCreationData();
+        Point5.Name = "point5"; // Fifth point
         
         TxRoboticViaLocationOperation FirstPoint = MyOp.CreateRoboticViaLocationOperation(Point1);
         TxRoboticViaLocationOperation SecondPoint = MyOp.CreateRoboticViaLocationOperationAfter(Point2, FirstPoint);
         TxRoboticViaLocationOperation ThirdPoint = MyOp.CreateRoboticViaLocationOperationAfter(Point3, SecondPoint);
+		TxRoboticViaLocationOperation FourthPoint = MyOp.CreateRoboticViaLocationOperationAfter(Point4, ThirdPoint);
+		TxRoboticViaLocationOperation FifthPoint = MyOp.CreateRoboticViaLocationOperationAfter(Point5, FourthPoint);
         
         // Impose a position to the new waypoint		
 		double rotVal = Math.PI;
@@ -65,7 +80,7 @@ public class MainScript
 		FirstPoint.AbsoluteLocation = rotX;
 		
 		var pointA = new TxTransformation(FirstPoint.AbsoluteLocation);
-		pointA.Translation = new TxVector(500, 0, 300);
+		pointA.Translation = new TxVector(650, -305, 100);
 		FirstPoint.AbsoluteLocation = pointA;
 		
 		// Impose a position to the second waypoint		
@@ -75,7 +90,7 @@ public class MainScript
 		SecondPoint.AbsoluteLocation = rotX2;
 		
 		var pointB = new TxTransformation(SecondPoint.AbsoluteLocation);
-		pointB.Translation = new TxVector(500, 0, 25);
+		pointB.Translation = new TxVector(650, -305, -7.14);
 		SecondPoint.AbsoluteLocation = pointB;
 		
 		// Impose a position to the third waypoint		
@@ -85,8 +100,28 @@ public class MainScript
 		ThirdPoint.AbsoluteLocation = rotX3;
 		
 		var pointC = new TxTransformation(ThirdPoint.AbsoluteLocation);
-		pointC.Translation = new TxVector(500, 0, 300);
+		pointC.Translation = new TxVector(650, -305, 300);
 		ThirdPoint.AbsoluteLocation = pointC;
+
+		// Impose a position to the fourth waypoint		
+		double rotVal4 = Math.PI;
+		TxTransformation rotX4 = new TxTransformation(new TxVector(rotVal4, 0, 0), 
+		TxTransformation.TxRotationType.RPY_XYZ);
+		FourthPoint.AbsoluteLocation = rotX4;
+		
+		var pointD = new TxTransformation(FourthPoint.AbsoluteLocation);
+		pointD.Translation = new TxVector(650, -305, -7.14);
+		FourthPoint.AbsoluteLocation = pointD;
+
+		// Impose a position to the fifth waypoint		
+		double rotVal5 = Math.PI;
+		TxTransformation rotX5 = new TxTransformation(new TxVector(rotVal5, 0, 0), 
+		TxTransformation.TxRotationType.RPY_XYZ);
+		FifthPoint.AbsoluteLocation = rotX5;
+		
+		var pointE = new TxTransformation(FifthPoint.AbsoluteLocation);
+		pointE.Translation = new TxVector(650, -305, 300);
+		FifthPoint.AbsoluteLocation = pointE;
 
 		// NOTE: you must associate the robot to the operation!
 		MyOp.Robot = robot; 
@@ -118,6 +153,60 @@ public class MainScript
         paramHandler.OnComplexValueChanged("Speed", new_speed, ThirdPoint);
         paramHandler.OnComplexValueChanged("Acc", new_accel, ThirdPoint);
 		paramHandler.OnComplexValueChanged("Zone", new_blend, ThirdPoint);
+
+		paramHandler.OnComplexValueChanged("Tool Frame", new_tcp, FourthPoint);
+		paramHandler.OnComplexValueChanged("Motion Type", new_motion_type, FourthPoint);
+        paramHandler.OnComplexValueChanged("Speed", new_speed, FourthPoint);
+        paramHandler.OnComplexValueChanged("Acc", new_accel, FourthPoint);
+		paramHandler.OnComplexValueChanged("Zone", new_blend, FourthPoint);
+
+
+		paramHandler.OnComplexValueChanged("Tool Frame", new_tcp, FifthPoint);
+		paramHandler.OnComplexValueChanged("Motion Type", new_motion_type, FifthPoint);
+        paramHandler.OnComplexValueChanged("Speed", new_speed, FifthPoint);
+        paramHandler.OnComplexValueChanged("Acc", new_accel, FifthPoint);
+		paramHandler.OnComplexValueChanged("Zone", new_blend, FifthPoint);
+
+		// Choose the point for the 'attach'
+		TxRoboticViaLocationOperation Waypoint1 =  TxApplication.ActiveDocument.
+  		GetObjectsByName("point2")[0] as TxRoboticViaLocationOperation;
+
+		// Choose the point for the 'detach'
+		TxRoboticViaLocationOperation Waypoint2 =  TxApplication.ActiveDocument.
+  		GetObjectsByName("point4")[0] as TxRoboticViaLocationOperation;
+
+		// Create the OLP command for attachment
+    	ArrayList elements1 = new ArrayList();
+		ArrayList elements2 = new ArrayList();
+	
+    	var myCmd1 = new TxRoboticCompositeCommandStringElement("# Attach ");	
+ 		var myCmd11 = new TxRoboticCompositeCommandTxObjectElement(item);
+		var myCmd111 = new TxRoboticCompositeCommandTxObjectElement(tool);
+
+		// Append all the command	
+    	elements1.Add(myCmd1);  
+		elements1.Add(myCmd11);  
+		elements1.Add(myCmd111); 
+
+		// Write the command 	
+    	TxRoboticCompositeCommandCreationData txRoboticCompositeCommandCreationData1 =
+    	new TxRoboticCompositeCommandCreationData(elements1);	
+    	Waypoint1.CreateCompositeCommand(txRoboticCompositeCommandCreationData1);	
+
+		// Create the OLP command for detachment
+    	var myCmd2 = new TxRoboticCompositeCommandStringElement("# Detach ");	
+ 		var myCmd21 = new TxRoboticCompositeCommandTxObjectElement(item);
+
+		// Append all the command	
+    	elements2.Add(myCmd2);  
+		elements2.Add(myCmd21);  
+
+
+		// Write the command 	
+    	TxRoboticCompositeCommandCreationData txRoboticCompositeCommandCreationData2 =
+    	new TxRoboticCompositeCommandCreationData(elements2);	
+    	Waypoint2.CreateCompositeCommand(txRoboticCompositeCommandCreationData2);
+
 		
                
     }
